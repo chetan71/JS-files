f53f79a2b893461f0c119123d647b789c83ea3f0
chetan71 <bhange71@gmail.com>
2024-05-21 08:18:09 +0530

    f

--- timeConversion.js
+++ timeConversion.js
@@ -1,9 +1,23 @@
+const s = "07:05:45PM";
+
 let sym = s.slice(-2);
 let frN = s.slice(0,2);
-console.log(s.slice(0, 2));
+let midT = s.slice(2, -2);
+// console.log(s.slice(0, 2));
 console.log(s.slice(0, -2))
 if (sym == "PM") {
-    if (s.slice(0, 2) == "12") {
-        console.log(s.slice(0, -2));
-    }else {frN = parseInt(frN)}
+    if (frN == "12") {
+        console.log(frN+midT);
+    }else {
+        frN = parseInt(frN) + 12;
+        console.log(frN.toString()+ midT);
+    }
+}else if (sym == "AM") {
+    if (frN == "12") {
+        frN = parseInt(frN)-12;
+        console.log(frN.toString()+midT);
+    }else {
+        
+        console.log(frN+ midT);
+    }
 }
