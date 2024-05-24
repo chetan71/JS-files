aa6022ffde9ca98c7d26adf1c0a86e2ed73881f4
chetan71 <91386497+chetan71@users.noreply.github.com>
2024-05-24 18:02:44 +0530

    d

--- asSort.js
+++ asSort.js
@@ -1,27 +1,33 @@
-const mat = [[9, 7, 8, 11, 21], [1, 4, 3, 7, 2], [4, 3, 14, 9, 12]]
-// console.log(sortCol(mat, 3, 5));
-sortCol(mat, 3, 5)
+const mat = [
+        [9, 7, 8, 11, 21],
+        [1, 4, 3, 7, 2],
+        [4, 3, 14, 9, 12]
+    ]
+    // console.log(sortCol(mat, 3, 5));
+console.log(mat[0][1]);
+// sortCol(mat, 3, 5)
+
 function sortCol(mat, N, M) {
     // Write your code here
-      let pick;
-      let flag = true;
-      for (let j = 0; j < mat[0].length; j++) {
-          let pos;
-          for (let i = 0; i < mat.length; i++) {
-              pick = mat[i][j];
-              let next = 0;
+    let pick;
+    let flag = true;
+    for (let j = 0; j < mat[0].length; j++) {
+        let pos;
+        for (let i = 0; i < mat.length; i++) {
+            pick = mat[i][j];
+            let next = 0;
             //   let next = mat[i+1][j];
-              console.log(mat[i+1][j]);
-              if (pick >next) {
-                  pick = next;
-                  pos = i+1;
-                  flag = false;
-              }
-          }
-          if (!flag) {
-                  mat[pos][j] = mat[0][j];
-                  mat[0][j] = pick
-              }
-      }
-      return mat;
-  }
+            console.log(mat[i + 1][j]);
+            if (pick > next) {
+                pick = next;
+                pos = i + 1;
+                flag = false;
+            }
+        }
+        if (!flag) {
+            mat[pos][j] = mat[0][j];
+            mat[0][j] = pick
+        }
+    }
+    return mat;
+}
---
+++ insertionSort.js
@@ -0,0 +1,10 @@
+const arr = [7, 6, 8, 5, 4, 9];
+console.log(arr.join(" "));
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//         const element = array[j];
+//         const element = array[i];
+//     }
+
+// }
